d488bbb7679f945d62021bef758187eba653d472
Matt Pryor <matt@stackhpc.com>
2021-09-28 12:39:55 +0100

    Improved run script

--- iperf/run.sh
+++ iperf/run.sh
@@ -1,22 +1,40 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 #####
 ## Run the iperf performance test
 #####
 
-helm repo add perftest https://stackhpc.github.io/kube-perftest
-helm repo update
+echo "[INFO] Installing kube-perftest helm repository"
+helm repo add perftest https://stackhpc.github.io/kube-perftest > /dev/null
+helm repo update > /dev/null
 
 # Generate a name for the job
 JOBNAME="perftest-iperf-$(cat /dev/urandom | tr -dc 'a-z0-9' | head -c 5)"
 
 # Run the job by installing the helm chart and get the chart name
-helm install $JOBNAME perftest/iperf --devel --set fullnameOverride=$JOBNAME --wait --wait-for-jobs
+echo "[INFO] Launching job - $JOBNAME"
+helm install $JOBNAME perftest/iperf \
+  --devel \
+  --wait \
+  --wait-for-jobs \
+  > /dev/null
+
+serverlabels="app.kubernetes.io/instance=$JOBNAME,app.kubernetes.io/component=server"
+clientlabels="app.kubernetes.io/instance=$JOBNAME,app.kubernetes.io/component=client"
+
+# Print information about where the pods were scheduled
+servernode="$(kubectl get po -l $serverlabels -o go-template='{{ (index .items 0).spec.nodeName }}')"
+echo "[INFO] Server scheduled on node - $servernode"
+clientnode="$(kubectl get po -l $clientlabels -o go-template='{{ (index .items 0).spec.nodeName }}')"
+echo "[INFO] Client scheduled on node - $clientnode"
 
 # Print the logs from the server
-kubectl logs deployment/$JOBNAME-server
+kubectl logs -l $serverlabels
 
 # Delete the resources for the job
-helm delete $JOBNAME
+echo "[INFO] Cleaning up job resources"
+helm delete $JOBNAME > /dev/null
+
+echo "[DONE]"
