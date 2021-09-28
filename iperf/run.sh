7c6ce408f3e8d8de25373e214814745ffb2a0b21
Matt Pryor <matt@stackhpc.com>
2021-09-28 11:49:27 +0100

    Add script to run iperf job

---
+++ iperf/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -ex
+
+#####
+## Run the iperf performance test
+#####
+
+helm repo add perftest https://stackhpc.github.io/
+helm repo update
+
+# Generate a name for the job
+JOBNAME="perftest-iperf-$(cat /dev/urandom | tr -dc 'a-z0-9' | head -c 5)"
+
+# Run the job by installing the helm chart and get the chart name
+helm install $JOBNAME perftest/iperf --devel --set fullnameOverride=$JOBNAME --wait --wait-for-jobs
+
+# Print the logs from the server
+kubectl logs deployment/$JOBNAME-server
+
+# Delete the resources for the job
+helm delete $JOBNAME
