b2af7727848b8cf7f9a49a724c7b7085f7fbc3d4
Matt Pryor <matt@stackhpc.com>
2021-09-28 13:43:20 +0100

    Prevent the swallowing of stderr

--- iperf/run.sh
+++ iperf/run.sh
@@ -7,8 +7,8 @@ set -e
 #####
 
 echo "[INFO] Installing kube-perftest helm repository"
-helm repo add perftest https://stackhpc.github.io/kube-perftest >/dev/null 2>&1
-helm repo update >/dev/null 2>&1
+helm repo add perftest https://stackhpc.github.io/kube-perftest >/dev/null
+helm repo update >/dev/null
 
 # Generate a name for the job
 JOBNAME="perftest-iperf-$(cat /dev/urandom | tr -dc 'a-z0-9' | head -c 5)"
@@ -26,24 +26,24 @@ helm install $JOBNAME perftest/iperf \
   $setclientargs \
   --wait \
   --wait-for-jobs \
-  >/dev/null 2>&1
+  >/dev/null
 
 serverlabels="app.kubernetes.io/instance=$JOBNAME,app.kubernetes.io/component=server"
 clientlabels="app.kubernetes.io/instance=$JOBNAME,app.kubernetes.io/component=client"
 
 # Print information about where the pods were scheduled
-servernode="$(kubectl get po -l $serverlabels -o go-template='{{ (index .items 0).spec.nodeName }}' 2>/dev/null)"
-servernodeip="$(kubectl get po -l $serverlabels -o go-template='{{ (index .items 0).status.hostIP }}' 2>/dev/null)"
+servernode="$(kubectl get po -l $serverlabels -o go-template='{{ (index .items 0).spec.nodeName }}')"
+servernodeip="$(kubectl get po -l $serverlabels -o go-template='{{ (index .items 0).status.hostIP }}')"
 echo "[INFO] Server scheduled on node - $servernode ($servernodeip)"
-clientnode="$(kubectl get po -l $clientlabels -o go-template='{{ (index .items 0).spec.nodeName }}' 2>/dev/null)"
-clientnodeip="$(kubectl get po -l $clientlabels -o go-template='{{ (index .items 0).status.hostIP }}' 2>/dev/null)"
+clientnode="$(kubectl get po -l $clientlabels -o go-template='{{ (index .items 0).spec.nodeName }}')"
+clientnodeip="$(kubectl get po -l $clientlabels -o go-template='{{ (index .items 0).status.hostIP }}')"
 echo "[INFO] Client scheduled on node - $clientnode ($clientnodeip)"
 
 # Print the logs from the server
-kubectl logs -l $serverlabels 2>/dev/null
+kubectl logs -l $serverlabels
 
 # Delete the resources for the job
 echo "[INFO] Cleaning up job resources"
-helm delete $JOBNAME >/dev/null 2>&1
+helm delete $JOBNAME >/dev/null
 
 echo "[DONE]"
