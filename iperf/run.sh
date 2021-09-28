560c8948f9290ddc9c5d6bb0ff5321f75bd2ce69
Matt Pryor <matt@stackhpc.com>
2021-09-28 12:50:01 +0100

    Swallow errors in script

--- iperf/run.sh
+++ iperf/run.sh
@@ -7,8 +7,8 @@ set -e
 #####
 
 echo "[INFO] Installing kube-perftest helm repository"
-helm repo add perftest https://stackhpc.github.io/kube-perftest > /dev/null
-helm repo update > /dev/null
+helm repo add perftest https://stackhpc.github.io/kube-perftest >/dev/null 2>&1
+helm repo update >/dev/null 2>&1
 
 # Generate a name for the job
 JOBNAME="perftest-iperf-$(cat /dev/urandom | tr -dc 'a-z0-9' | head -c 5)"
@@ -19,22 +19,24 @@ helm install $JOBNAME perftest/iperf \
   --devel \
   --wait \
   --wait-for-jobs \
-  > /dev/null
+  >/dev/null 2>&1
 
 serverlabels="app.kubernetes.io/instance=$JOBNAME,app.kubernetes.io/component=server"
 clientlabels="app.kubernetes.io/instance=$JOBNAME,app.kubernetes.io/component=client"
 
 # Print information about where the pods were scheduled
-servernode="$(kubectl get po -l $serverlabels -o go-template='{{ (index .items 0).spec.nodeName }}')"
-echo "[INFO] Server scheduled on node - $servernode"
-clientnode="$(kubectl get po -l $clientlabels -o go-template='{{ (index .items 0).spec.nodeName }}')"
-echo "[INFO] Client scheduled on node - $clientnode"
+servernode="$(kubectl get po -l $serverlabels -o go-template='{{ (index .items 0).spec.nodeName }}' 2>/dev/null)"
+servernodeip="$(kubectl get po -l $serverlabels -o go-template='{{ (index .items 0).status.hostIP }}' 2>/dev/null)"
+echo "[INFO] Server scheduled on node - $servernode ($servernodeip)"
+clientnode="$(kubectl get po -l $clientlabels -o go-template='{{ (index .items 0).spec.nodeName }}' 2>/dev/null)"
+clientnodeip="$(kubectl get po -l $clientlabels -o go-template='{{ (index .items 0).status.hostIP }}' 2>/dev/null)"
+echo "[INFO] Client scheduled on node - $clientnode ($clientnodeip)"
 
 # Print the logs from the server
-kubectl logs -l $serverlabels
+kubectl logs -l $serverlabels 2>/dev/null
 
 # Delete the resources for the job
 echo "[INFO] Cleaning up job resources"
-helm delete $JOBNAME > /dev/null
+helm delete $JOBNAME >/dev/null 2>&1
 
 echo "[DONE]"
