3ef9e61b9341429a06280e0a03dd91c304cbb429
Matt Pryor <matt@stackhpc.com>
2021-10-08 15:28:40 +0100

    Add debug statements to launch script

--- images/fio/fio-run.sh
+++ images/fio/fio-run.sh
@@ -8,8 +8,11 @@
 set -e
 
 CONF_DIRECTORY="${CONF_DIRECTORY:-/fio}"
+echo "[INFO] Conf directory - $CONF_DIRECTORY"
+
 if [ -d "$CONF_DIRECTORY" ]; then
     JOB_FILES="$(find $CONF_DIRECTORY -mindepth 1 -maxdepth 1 -type f)"
 fi
+echo "[INFO] Job files - $JOB_FILES"
 
 exec fio "$@" $JOB_FILES
