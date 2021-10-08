93ec9451df077b90404bc99550c6576e4d0c851b
Matt Pryor <matt@stackhpc.com>
2021-10-08 15:39:27 +0100

    Remove debugging statements

--- images/fio/fio-run.sh
+++ images/fio/fio-run.sh
@@ -8,13 +8,7 @@
 set -e
 
 CONF_DIRECTORY="${CONF_DIRECTORY:-/fio}"
-echo "[INFO] Conf directory - $CONF_DIRECTORY"
-ls -lR $CONF_DIRECTORY
-
 if [ -d "$CONF_DIRECTORY" ]; then
-    echo "[INFO] Conf directory exists"
     JOB_FILES="$(find $CONF_DIRECTORY -mindepth 1 -maxdepth 1)"
 fi
-echo "[INFO] Job files - $JOB_FILES"
-
 exec fio "$@" $JOB_FILES
