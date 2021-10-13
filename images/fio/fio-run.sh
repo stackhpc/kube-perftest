40fbbf95d8e3e76ff43a19e0ed69f94b837fb2fe
Matt Pryor <matt@stackhpc.com>
2021-10-13 12:48:41 +0100

    Rework wrapper script to include client sync

--- images/fio/fio-run.sh
+++ images/fio/fio-run.sh
@@ -1,37 +1,52 @@
 #!/usr/bin/env bash
 
 #####
-## Lightweight script that understands how to pass all the files from a
-## directory as jobs for fio
+## Script that understands how to execute fio as part of a Fio benchmark
+## with configuration from environment variables
 #####
 
 set -e
 
-# Allow the conf directory to come from an environment variable
-CONF_DIRECTORY="${CONF_DIRECTORY:-/fio}"
-if [ -d "$CONF_DIRECTORY" ]; then
-    JOB_FILES="$(find $CONF_DIRECTORY -mindepth 1 -maxdepth 1)"
+CONFIG_FILE="${CONFIG_FILE:-/fio/job.fio}"
+WORK_DIR="${WORK_DIR:-/scratch}"
+NUM_CLIENTS="${NUM_CLIENTS:-1}"
+
+# Benchmark and pod name are required
+if [ -z "$BENCHMARK_NAME" ]; then
+    echo "BENCHMARK_NAME is not set" 1>&2
+    exit 1
+fi
+if [ -z "$POD_NAME" ]; then
+    echo "POD_NAME is not set" 1>&2
+    exit 1
 fi
 
-# Extract the given directory from the arguments without consuming them
-DIRECTORY=
-for arg in "$@"; do
-    case "$arg" in
-        --directory=*)
-            DIRECTORY="${arg#*=}"
-            ;;
-    esac
+# Derive the lock and data directories
+LOCK_DIR="${WORK_DIR}/${BENCHMARK_NAME}.lock"
+DATA_DIR="${WORK_DIR}/${POD_NAME}"
+
+# Wait for each client to make an entry in the lock directory before proceeding
+rm -rf $LOCK_DIR
+mkdir -p $LOCK_DIR
+while true; do
+    echo "READY" > "${LOCK_DIR}/${POD_NAME}"
+    if [ "$(ls $LOCK_DIR | wc -l)" -ge "$NUM_CLIENTS" ]; then
+        break
+    fi
+    sleep 1
 done
 
-# Make sure the directory exists before starting
-if [ -n "$DIRECTORY" ]; then
-    mkdir -p $DIRECTORY
-fi
+# Once the clients have synchronised, remove the lock directory
+# This prevents clients which error out from successfully restarting unless ALL the
+# clients error out and restart, hence preventing the overall job from succeeding
+# but not actually executing N clients in parallel as expected
+rm -rf $LOCK_DIR
 
-# Execute FIO with the given arguments
-fio "$@" $JOB_FILES
+# Make the directory to create test resources in
+mkdir -p $DATA_DIR
+
+# Execute fio
+fio $CONFIG_FILE --directory=$DATA_DIR --output=/dev/stdout --output-format=json+
 
 # Clean up the directory afterwards
-if [ -n "$DIRECTORY" ]; then
-    rm -rf "$DIRECTORY"
-fi
+rm -rf $DATA_DIR
